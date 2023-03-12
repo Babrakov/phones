8b92ee0f4e2f4a4a8d443234e8172a21a73be87c
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-03-12 14:28:42 +0100

    own scripts

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -129,6 +129,7 @@ return [
     'scripts' => [
         // Backstrap includes jQuery, Bootstrap, CoreUI, PNotify, Popper
         'packages/backpack/base/js/bundle.js',
+        'js/scripts.js',
 
         // examples (everything inside the bundle, loaded from CDN)
         // 'https://code.jquery.com/jquery-3.4.1.min.js',
